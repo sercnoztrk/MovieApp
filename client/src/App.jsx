6d8d0f3be6f1c66a459cef01f2848cd0a11f296d
N. Sercan Ozturk <n.sercan.ozturk@gmail.com>
2023-07-08 00:44:49 +0300

    Fix react routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "./components/navbar/Navbar";
 import SideBar from "./components/Sidebar/SideBar";
+import Explore from "./pages/Explore";
 import MoviePage from "./pages/moviePage/MoviePage";
 import MoviesPage from "./pages/moviesPage/MoviesPage";
 import "./styles/globalStyles.css";
@@ -15,35 +16,21 @@ function App() {
     if (mode === "light") { return document.documentElement.classList.remove("dark") }
     else { document.documentElement.classList.add("dark") }
   }, [mode]);
+  
   return (
     <BrowserRouter>
       <div onClick={() => setIsSearchItemsShow(false)} className={`${ !isSearchItemsShow && "hidden" } fixed z-40 w-full h-full bg-black backdrop-blur-sm dark:bg-opacity-60 bg-opacity-25`}/>
         <div onClick={() => setOpenMenu(false)} className={`${ !openMenu && "hidden" } fixed z-[51] w-full h-full bg-black lg:hidden backdrop-blur-sm dark:bg-opacity-70 bg-opacity-25`}/>
           <div className={`dark:text-textDark text-textLight ${mode === "dark" ? "gradient-06" : "lightTheme"} `}>
-            <NavBar
-              isSearch={isSearchItemsShow}
-              setIsSearch={setIsSearchItemsShow}
-              setMode={setMode}
-              mode={mode}
-              openMenu={openMenu}
-              setOpenMenu={setOpenMenu}
-            />
+            <NavBar isSearch={isSearchItemsShow} setIsSearch={setIsSearchItemsShow} setMode={setMode} mode={mode} openMenu={openMenu} setOpenMenu={setOpenMenu}/>
             <div className="flex">
-              <SideBar
-                mode={mode}
-                openMenu={openMenu}
-                setOpenMenu={setOpenMenu}
-                setMode={setMode}
-              />
+              <SideBar mode={mode} openMenu={openMenu} setOpenMenu={setOpenMenu} setMode={setMode}/>
               <div className="w-full flex flex-col">
-                <Switch>
-                  <Route path={"/series"} component={() => <SeriesPage />} />
-                  <Route path={"/movies/:id"} exact component={() => <MoviePage />}/>
-                  <Route path={"/genres/:id"} exact component={() => <MoviesByGenre />}/>
-                  <Route path={"/movies"} exact component={() => <MoviesPage />} />
-                  <Route path={"/genres"} exact component={() => <Genres />} />
-                  <Route path={"/"} exact component={() => <Explore />} />
-                </Switch>
+                <Routes>
+                  <Route path="movies/:id" element={<MoviePage />}/>
+                  <Route path="movies" element={<MoviesPage />} />
+                  <Route path="/" element={<Explore />} />
+                </Routes>
               </div>
             </div>
           </div>
